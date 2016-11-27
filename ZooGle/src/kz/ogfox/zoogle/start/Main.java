29867c08f02cf09d19397967772e019b10310bc4
OGFoxing <pala4ik93@icloud.com>
2016-11-27 16:00:06 +0700

    add fxml and login form

--- ZooGle/src/kz/ogfox/zoogle/start/Main.java
+++ ZooGle/src/kz/ogfox/zoogle/start/Main.java
@@ -1,19 +1,30 @@
 package kz.ogfox.zoogle.start;
 	
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
+			FXMLLoader fxmlLoader = new FXMLLoader();
+			fxmlLoader.setLocation(getClass().getResource("../fxml/autorization.fxml"));
+			fxmlLoader.setResources(ResourceBundle.getBundle("bundles.Locale",new Locale("en")));
+			
+			Parent fxmlMain = fxmlLoader.load();
+			
+			primaryStage.setResizable(false);
+			primaryStage.setTitle(fxmlLoader.getResources().getString("title"));
+			primaryStage.setScene(new Scene(fxmlMain,400,300));
+			primaryStage.getIcons().add(new Image("file:icon/fabric-letter-z-black.jpg"));
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
